452b19938fbc4330ee470ad642a6a1038b5516e1
Alex <59831659+alt-ee@users.noreply.github.com>
2023-05-06 18:48:58 +0100

    Updated wezterm config to match style of latest documentation

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,16 @@
-local wezterm = require 'wezterm';
-return{
-    font = wezterm.font("Iosevka"),
-    color_scheme = "Gruvbox Dark",
+local wezterm = require 'wezterm'
+local config = {}
 
-    font_size = 15,
-    use_fancy_tab_bar = false,
-    hide_tab_bar_if_only_one_tab = true,
-    audible_bell = "Disabled",
-}
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.font = wezterm.font 'Iosevka Comfy'
+config.color_scheme = 'Borland'
+
+config.font_size = 14
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.audible_bell = 'Disabled'
+
+return config
