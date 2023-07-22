8ec6052d756db273e1ed0352bffa75d3160abc3b
Alex <59831659+alt-ee@users.noreply.github.com>
2023-07-22 22:17:29 +0100

    Removed unneeded config

--- .config/crafted-emacs/custom-modules/crafted-c.el
+++
@@ -1,5 +0,0 @@
-(require 'eglot)
-(add-to-list 'eglot-server-programs '((c++-mode c-mode) "/usr/bin/clangd" "-log=verbose"))
-(setq-default c-basic-offset 4)
-
-(provide 'crafted-c)
--- .config/crafted-emacs/early-config.el
+++
@@ -1,8 +0,0 @@
-;;; early-config.el --- Early init customizations for crafted emacs  -*- lexical-binding: t; -*-
-
-(custom-set-faces (backquote (default ((t (:family "Iosevka" :height 130))))))
-
-(setf default-frame-alist (assq-delete-all 'background-color default-frame-alist))
-(setf default-frame-alist (assq-delete-all 'foreground-color default-frame-alist))
-
-(provide 'early-config)
