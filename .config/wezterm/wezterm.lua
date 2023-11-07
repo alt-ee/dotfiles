2df7fef4fbe22ca8329990cb3915d5a4256a50ce
Alex <59831659+alt-ee@users.noreply.github.com>
2023-11-07 22:57:59 +0000

    Some wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,12 +5,13 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.font = wezterm.font 'Iosevka Comfy'
-config.color_scheme = 'Borland'
-
-config.font_size = 14
+config.font_size = 12
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.audible_bell = 'Disabled'
+config.check_for_updates = false
+config.show_update_window = false
+config.font = wezterm.font 'Iosevka Term'
+config.default_prog = { '/usr/bin/fish', '-i' }
 
 return config
